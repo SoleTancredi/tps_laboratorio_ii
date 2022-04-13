b06be5c37ae5c6bcf0bb32ac60ee4429a21ffc0f
SoleTancredi <tancredi620@gmail.com>
2022-04-13 15:45:47 -0300

    creacion del proyecto

---
+++ TP1/Tancredi.Soledad.2C.TP1/Tancredi.Soledad.2C.TP1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tancredi.Soledad._2C.TP1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
